fab56fb4d63758421832aca302a81cdcb200f8a6
sai prabhu <gsaiprabhu1239@gmail.com>
2024-10-06 17:34:09 +0530

    changed the names

--- main.tf
+++ main.tf
@@ -25,12 +25,12 @@ resource "azurerm_kubernetes_cluster" "aks-cls" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  value     = azurerm_kubernetes_cluster.aks-cls.kube_config[0].client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks-cls.kube_config_raw
 
   sensitive = true
 }
